53879fa62dbded057c4b43840c2cfc532d5f0f66
Chaithanya876 <108938522+Chaithanya876@users.noreply.github.com>
2025-12-26 06:16:53 +0530

    udpated poll

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Poll SCM");
+        System.out.println("Poll SCM Jenkins");
     }
 }
 
