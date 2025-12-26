d4a0c0933c4e787f53e01c9a0d92f69ed5ada407
Chaithanya876 <108938522+Chaithanya876@users.noreply.github.com>
2025-12-26 06:55:26 +0530

    weebhook

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Poll SCM");
-        System.out.println("Poll SCM Jenkins");
+        System.out.println("Weebhook");
         System.out.println("lines");
     }
 }
